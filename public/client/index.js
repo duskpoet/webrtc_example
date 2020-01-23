752a23560c850392f256d4c4cd36d6d6cddea295
Eduard Dyckman <eduarddyckman@yandex-team.ru>
2020-01-23 18:15:36 +0300

    First call

--- public/client/index.js
+++ public/client/index.js
@@ -1,8 +1,66 @@
 const constraints = {
-    audio: true,
-    video: true,
-}
+  audio: true,
+  video: true
+};
+const config = {
+  urls: "stun:stun.l.google.com:19302"
+};
+const pcIn = new RTCPeerConnection(config);
+const pcOut = new RTCPeerConnection(config);
+pcIn.ontrack = event => {
+  document.getElementById("them").srcObject = event.streams[0];
+};
+pcOut.ontrack = event => {
+  document.getElementById("them").srcObject = event.streams[0];
+};
 
-navigator.mediaDevices.getUserMedia(constraints).then((ms) => {
-    document.getElementById('me').srcObject = ms;
-})
+const socket = (window._socket = io("/"));
+
+window.submitName = () => {
+  const name = document.getElementById("name").value;
+  socket.emit("register", name);
+};
+
+socket.on("clients", clients => {
+  document.getElementById("participants").innerHTML = `<table>
+        ${Object.values(clients)
+          .map(
+            ({ name, id }) =>
+              `<tr>
+            <td>${name}</td>
+            <td><button onclick="callClient('${id}')">CALL</td>
+          </tr>`
+          )
+          .join("")}
+    </table>`;
+});
+
+pcIn.onicecandidate = ({ candidate }) =>  socket.emit('in candidate', candidate);
+pcOut.onicecandidate = ({ candidate }) =>  socket.emit('in candidate', candidate);
+window.callClient = async id => {
+  const ms = await navigator.mediaDevices.getUserMedia(constraints)
+  document.getElementById("me").srcObject = ms;
+  ms.getTracks().forEach(track => {
+    pcOut.addTrack(track, ms);
+  });
+  const offer = await pcOut.createOffer();
+  await pcOut.setLocalDescription(offer);
+  socket.emit("call offer", { id, offer });
+};
+socket.on("incoming offer", async ({offer, id}) => {
+  await pcIn.setRemoteDescription(offer);
+  const ms = await navigator.mediaDevices.getUserMedia(constraints)
+  document.getElementById("me").srcObject = ms;
+  ms.getTracks().forEach(track => {
+    pcIn.addTrack(track, ms);
+  });
+  await pcIn.setLocalDescription(await pcIn.createAnswer())
+  socket.emit('call answer', { id, offer: pcIn.localDescription });
+});
+socket.on('incoming answer', ({id, offer}) => {
+  pcOut.setRemoteDescription(offer);
+});
+socket.on('out candidate', (candidate) => {
+  pcIn.addIceCandidate(candidate);
+  pcOut.addIceCandidate(candidate);
+});
--- server.js
+++ server.js
@@ -1,6 +1,44 @@
-const express = require('express')
-
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+app.use(express.static("public"));
 
-app.listen(3000);
+const clients = {};
+const clientsPool = {};
+io.on("connection", socket => {
+  clientsPool[socket.id] = socket;
+  socket.on("register", name => {
+    clients[socket.id] = { name, id: socket.id };
+    sendClients();
+  });
+  socket.on("disconnect", () => {
+    delete clients[socket.id];
+    delete clientsPool[socket.id];
+    sendClients();
+  });
+  socket.on("call offer", ({ id, offer }) => {
+    clientsPool[id].emit("incoming offer", { offer, id: socket.id });
+  });
+  socket.on("in candidate", candidate => {
+    Object.values(clientsPool)
+      .filter(({ id }) => id !== socket.id)
+      .forEach(socket => {
+        socket.emit("out candidate", candidate);
+      });
+  });
+  socket.on("call answer", ({ id, offer }) => {
+    clientsPool[id].emit("incoming answer", { id: socket.id, offer });
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log("Listening on " + port);
+});
+
+function sendClients() {
+  Object.values(clientsPool).forEach(socket => {
+    socket.emit("clients", clients);
+  });
+}
